9b400d74e4993b6cd43c4a45f03c32a594dfb6b0
Mirelly Almeida <136247614+Mihvieira@users.noreply.github.com>
2024-08-18 22:23:43 -0400

    tabela organizada e tradução corrigida

--- apiAvatarComDjango/avatar/app/templatetags/app_extras.py
+++ apiAvatarComDjango/avatar/app/templatetags/app_extras.py
@@ -8,22 +8,43 @@ tradutor = Translator()
 
 @register.filter(name="get_item")
 def get_item(dictionary, key):
-    dado = dictionary.get(key)
-    return traduzir_item(dado)
+    if key == 'photoUrl':
+        return dictionary.get(key)
+    else:
+        dado = dictionary.get(key)
+        return traduzir_item(dado)
 
-# TODO avaliar adição de exceções 
+
 @register.filter(name='traduzir_item')
 def traduzir_item(valor):
     try:
-        if type(valor) is list:
+        if len(valor) == 0:
+            return 0
+        
+        elif valor == 'Bum-Ju':
+            return valor
+        
+        elif type(valor) is list:
             lista = []
             for i in valor:
-                traduzir = tradutor.translate(i,dest= 'pt').text
-                lista.append(traduzir)
-            return lista
-        else:
+                if len(i) > 5:
+                    traduzir = tradutor.translate(i,dest= 'pt').text
+                    lista.append(traduzir.title())
+                else:
+                    lista.append(i.title())
+            
+            if len(lista) > 1:
+                return ', '.join(str(element) for element in lista)
+            else:
+                return lista[0]
+        
+        elif len(valor) > 5:
             traduzir = tradutor.translate(valor,dest= 'pt').text
-            return traduzir            
+            return traduzir.title()
+        
+        else:
+            return valor
+             
     except:
         return valor
 
