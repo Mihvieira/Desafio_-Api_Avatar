9db03b03fae3971eb42acfd04364f4d396352cf7
Mirelly Almeida <136247614+Mihvieira@users.noreply.github.com>
2024-08-20 11:27:24 -0400

    Update views.py

--- apiAvatarComDjango/avatar/app/views.py
+++ apiAvatarComDjango/avatar/app/views.py
@@ -1,41 +1,18 @@
 from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 import requests
 import json
-from googletrans import Translator
 
-
-def transformarDados(informacoes):
-    personagens = {}
-    tradutor = Translator()
-    
-    for item, valor in enumerate(informacoes):
-        for chave in valor:
-            if chave == '_id':
-                None
-            else:
-                traducaoColunas = tradutor.translate(chave,dest= 'pt').text
-                dado = valor.get(chave) #retorna os dados da chave
-                
-                if type(dado) == list:
-                    listaDados = []
-                    for i in dado:
-                        traducaoDados = tradutor.translate(i,dest= 'pt').text
-                        listaDados.append(traducaoDados)
-                    personagens[traducaoColunas]=listaDados
-                else:
-                    traducaoDados = tradutor.translate(dado,dest= 'pt').text
-                    personagens[traducaoColunas]=traducaoDados
-    
-    return personagens
 
 def index(request):
-    url = "https://last-airbender-api.fly.dev/api/v1/characters"
+    pagina = request.GET.get('page', 1)
+    quantidade_por_pagina = 10
+    url = f'https://last-airbender-api.fly.dev/api/v1/characters?perPage={quantidade_por_pagina}&page={pagina}'
     requisicao = requests.get(url)
     dados = requisicao.json()
     dicionario = {}
     for item, valor in enumerate(dados):
-        dicionario[item] = valor
+        dicionario[item] = valor    
     
     context = {
         'personagens' : dicionario,
